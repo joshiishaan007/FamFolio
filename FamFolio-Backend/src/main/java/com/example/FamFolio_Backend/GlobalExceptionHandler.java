e0d8f8a50d0deea8cba0344671c9bdd1110cb1fe
YashGokulgandhi <yashgokulgandhi6@gmail.com>
2025-05-06 16:16:07 +0530

    Global Exception Handler created

---
+++ FamFolio-Backend/src/main/java/com/example/FamFolio_Backend/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.example.FamFolio_Backend;
+
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
